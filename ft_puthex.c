89ad1b8af965dbde345d1b1c452b567aa9c076ed
decabral <deborahcabralsilva@gmail.com>
2025-12-17 10:42:33 -0300

    ft_puthex

--- ft_puthex.c
+++ ft_puthex.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_puthex.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: decabral <decabral@student.42.rio>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/21 10:50:18 by decabral          #+#    #+#             */
-/*   Updated: 2025/12/05 10:53:38 by decabral         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "ft_printf.h"
 
@@ -31,8 +20,3 @@ int	ft_puthex(unsigned long n, char format)
 	count++;
 	return (count);
 }
-/*#include <stdio.h>
-int main (void)
-{
-	printf("%d\n", ft_puthex(123456789, 'x'));
-}*/
